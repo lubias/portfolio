e134d3a56fa640918f6353f95e0b75de2177076d
Luana <santos.luanabeatriz1995@gmail.com>
2023-07-14 18:57:47 +0100

    Criada estrutura do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/section/Navbar';
+import Presentation from './components/section/Presentation';
+import Projects from './components/section/Projects';
+import Skils from './components/section/Skils';
+import Footer from './components/section/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Presentation />
+      <Skils />
+      <Projects />
+      <Footer />
+    </>
   );
 }
 
