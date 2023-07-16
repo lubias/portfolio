fb49f59c32edb9c4cf2d8b9609d15951b017dc3f
Luana <santos.luanabeatriz1995@gmail.com>
2023-07-16 21:54:37 +0100

    Criado componente do footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import './index.scss';
 
 function Footer() {
   return (
-    <div><h1>Footer</h1></div>
-  )
+    <div className="Footer">
+      <ul className="Navbar__ul">
+        <li className="Navbar__li">
+          <a
+            href="https://www.linkedin.com/in/luana-beatriz-dos-santos-4095b8107/"
+            target="_blank"
+          >
+            <FaLinkedin size={30} className="icones" />
+          </a>
+        </li>
+        <li className="Navbar__li">
+          <a href="https://github.com/lubias" target="_blank">
+            <FaGithub size={30} className="icones" />
+          </a>
+        </li>
+      </ul>
+      <p><a href="mailto:luanabia_s@live.com">luanabia_s@live.com</a></p>
+      <p>Luana Beatriz © 2023</p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
